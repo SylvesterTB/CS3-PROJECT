32af6e2c58ad30039a83eef5f295a131bbbcbf3d
7002579 <lyracat123@gmail.com>
2025-01-27 14:49:02 -0500

    Canvas grid

--- canvas.js
+++ canvas.js
@@ -1,29 +1,34 @@
-var container = document.getElementById("container");
+var canvas = document.getElementById("canvas");
 var isDrawing = false;
-var currentColor = "black";
 var borderStuff = "";
 var clear = false;
 
-function makeRows() {
-    container.style.setProperty('--grid-rows', 100);
-    container.style.setProperty('--grid-cols', 100);
-    for (c = 0; c < (100 * 100); c++) {
-        let cell = document.createElement("div");
 
-        // Change to click event
+function makeCanvasRows() {
+    canvas.style.setProperty('--canvas-grid-rows', "20");
+    canvas.style.setProperty('--canvas-grid-cols', "20");
+    for (c = 0; c < (20 * 20); c++) {
+        let cell = document.createElement("div");
         cell.addEventListener("mousedown", mouseDown)
         cell.addEventListener("mouseover", mouseOver)
         cell.addEventListener("mouseup", mouseUp)
-        container.appendChild(cell).className = "grid-item";
-    };
+        canvas.appendChild(cell).className = "canvas-grid-item";
+    }
 
-};
+}
 
-// Rename this function if you want (optional)
+function getColor(element) {
+    currentColor = element.style.backgroundImage;
+    currentColor = currentColor.substring(currentColor.indexOf("rgb(")+4,currentColor.indexOf(")"));
+    console.log("currentColor: " + currentColor);
+
+}
+
 function mouseDown() {
     this.style.backgroundColor = currentColor;
     // this.addEventListener("mouseover", mouseOver)
     isDrawing = true
+    console.log("dasdasd")
 }
 
 function mouseOver() {
@@ -38,29 +43,8 @@ function mouseUp() {
     isDrawing = false
 }
 
-function changeRed()
-{
-    currentColor = "red"
-    let currentElement = document.getElementById("selectRed")
-    console.log(currentElement)
-    currentElement.style.border = "blue 2px solid"
-    // this.focus();
-}
+var currentColor = "Black";
 
 
-function clearScreen() {
-    // console.log("clear")
-    var grid_select = document.getElementsByClassName("grid-item");
-    console.log(grid_select)
-    // const grid_select = document.querySelector("grid-item")
-    // containerSelect.style.backgroundColor ="white"
-    grid_select.style.backgroundColor = "green"
-    // grid_select.style.setProperty('--randomcolor', "blue");
 
-}
-
-function erase() {
-    currentColor = "white"
-}
-
-makeRows()
+makeCanvasRows()
--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-
-
-const colors = document.getElementById("colors");
-
-function makeRows() {
-    colors.style.setProperty('--grid-rows', 10);
-    colors.style.setProperty('--grid-cols', 2);
-
-    for (c = 0; c < (10 * 2); c++) {
-        let cell = document.createElement("div");
-        colors.appendChild(cell).className = "grid-item";
-        cell.innerText = (c+1);
-
-    }
-
-}
-
-// makeRows()
-
-
-
+// const colors = document.getElementById("colors");
+//
+// function makeRows() {
+//     colors.style.setProperty('--grid-rows', 10);
+//     colors.style.setProperty('--grid-cols', 2);
+//
+//     for (c = 0; c < (10 * 2); c++) {
+//         let cell = document.createElement("div");
+//         colors.appendChild(cell).className = "grid-item";
+//         cell.innerText = (c+1);
+//     }
+// }
+//
+// // makeRows()
